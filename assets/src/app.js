f23884fadebfc9d23c024451063c15efd725a722
pi3 <pi3@raspberry.net>
2017-02-20 17:38:32 +0000

    added gitignore and changed config

--- assets/src/app.js
+++ assets/src/app.js
@@ -1,3 +1,4 @@
+import config from './env.js';
 import ng from 'angular';
 import ng_ui_router from 'angular-ui-router';
 import firebase from 'firebase';
@@ -5,14 +6,6 @@ import ng_fire from 'angularfire';
 import MainRoute from './config/main_route.js';
 import firebase_config from './config/firebase_config.js';
 import contactsComponent from './components/contacts/contacts.component.js';
-
-var config = {
-    apiKey: "AIzaSyAxrD8WjUf2QtabsX5dONZKxDQdjRQ3GDY",
-    authDomain: "mycontacts-24bd0.firebaseapp.com",
-    databaseURL: "https://mycontacts-24bd0.firebaseio.com",
-    storageBucket: "mycontacts-24bd0.appspot.com",
-    messagingSenderId: "1026929907954"
-};
 
 firebase.initializeApp(config);
 
